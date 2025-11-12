f42da6ca538d88af9701cf692b6fd121b8e077f1
DELL E5490 <vontedduramadevi@gmail.com>
2025-11-12 06:09:30 +0530

    springcore properties file & strategy pattern

--- springcoreweatherdemo/src/main/java/com/springcore/propertiesfile/client/PropertiesFileTest.java
+++ springcoreweatherdemo/src/main/java/com/springcore/propertiesfile/client/PropertiesFileTest.java
@@ -2,8 +2,10 @@ package com.springcore.propertiesfile.client;
 
 import com.springcore.propertiesfile.beans.PersonInfo;
 import com.springcore.propertiesfile.config.AppConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@Slf4j
 public class PropertiesFileTest
 {
     public static void main(String[] args) {
@@ -11,6 +13,6 @@ public class PropertiesFileTest
         AnnotationConfigApplicationContext ctx=new AnnotationConfigApplicationContext(AppConfig.class);
         //get spring bean class obj ref
         PersonInfo info=ctx.getBean("pInfo",PersonInfo.class);
-        System.out.println(info);
+       log.info(info.toString());
     }
 }
