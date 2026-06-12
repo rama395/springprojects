b3aeddfd17f91bd26480d8ffcf20ed840c8cd820
DELL E5490 <vontedduramadevi@gmail.com>
2026-06-12 10:37:55 +0530

    Arrayprograms

--- Miniproject/CloudAPIGateway/corejavapractice/src/javaprograms/arrays/RemovingDuplicateElements.java
+++ Miniproject/CloudAPIGateway/corejavapractice/src/javaprograms/arrays/RemovingDuplicateElements.java
@@ -1,23 +1,60 @@
 package javaprograms.arrays;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class RemovingDuplicateElements
 {
     public static void main(String[] args) {
         int[] arr={4,2,3,1,2,4,5};
-        removeDuplicatesUsingHashSet(arr);
-
+        int[] arr2=removeDuplicatesUsingHashSet(arr);
+        System.out.println(Arrays.toString(arr2));
+        int[] arr3=removeDuplicatesWithoutUsingHashSet(arr);
+        System.out.println(Arrays.toString(arr3));
     }
-    private static void removeDuplicatesUsingHashSet(int[] arr)
+    private static int[] removeDuplicatesUsingHashSet(int[] arr)
     {
         Set<Integer> set=new HashSet<>();
         for (int num:arr)
         {
             set.add(num);
         }
-        System.out.println(set);
+       // System.out.println(set);
+        int[] arr1=new int[set.size()];
+        int i=0;
+        for (int ele:set)
+        {
+            arr1[i]=ele;
+            i++;
+        }
+        return arr1;
+    }
+    private static int[] removeDuplicatesWithoutUsingHashSet(int[] arr)
+    {
+        List<Integer> list=new ArrayList<>();
+        for (int i=0;i< arr.length;i++)
+        {
+            boolean isDuplicate=false;
+            for (int j=i+1;j< arr.length;j++)
+            {
+                if (arr[i]==arr[j])
+                {
+                    isDuplicate=true;
+                }
+
+            }
+            if (!isDuplicate)
+            {
+                list.add(arr[i]);
+            }
+        }
+        int[] arr2=new int[list.size()];
+        int i=0;
+        for (int a:list)
+        {
+            arr2[i]=a;
+            i++;
+        }
+        return arr2;
     }
 
 }
