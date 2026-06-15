978ed5ed1dfd0ed547abf109825e0ea7cab52553
DELL E5490 <vontedduramadevi@gmail.com>
2026-06-15 15:47:14 +0530

    Java8Featuredpgrms

---
+++ Miniproject/CloudAPIGateway/corejavapractice/src/javaprograms/java8features/predicates/Test.java
@@ -0,0 +1,33 @@
+package javaprograms.java8features.predicates;
+
+import java.util.function.Predicate;
+
+public class Test
+{
+    public static void main(String[] args) {
+        int[] x={0,5,10,15,20,25,30};
+        Predicate<Integer> p1= i->i>10;
+        Predicate<Integer> p2=i->i%2==0;
+        System.out.println("Numbers greater than 10:");
+        m1(p1,x);
+        System.out.println("EVen numbers are:");
+        m1(p2,x);
+        System.out.println("Numbers not greater than 10:");
+        m1(p1.negate(),x);
+        System.out.println("Numbers greater than 10 and even:");
+        m1(p1.and(p2),x);
+        System.out.println("numbers greater than 10 or even:");
+        m1(p1.or(p2),x);
+
+    }
+    public static void m1(Predicate<Integer>p,int[]x)
+    {
+        for (int x1:x)
+        {
+            if (p.test(x1))
+            {
+                System.out.println(x1);
+            }
+        }
+    }
+}
